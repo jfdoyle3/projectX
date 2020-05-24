ceaddc805289197afbeb963078e6c6d8a5f942ec
Jim Doyle <indellibleink@gmail.com>
2020-05-23 20:57:18 -0400

    got json string to json but not quite right

--- DataProcessing/src/GetAPIData/PotHoleData.java
+++ DataProcessing/src/GetAPIData/PotHoleData.java
@@ -1,22 +1,26 @@
 package GetAPIData;
 
-
-
 import com.google.gson.Gson;
-
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
+import kong.unirest.json.JSONObject;
 
 public class PotHoleData {
 
 	public static void getPotHoleData() {
 		
 		final HttpResponse<String> jsonResponse = Unirest.get("https://data.providenceri.gov/resource/tisk-wsvu.json").asString();
+		String stringJson=jsonResponse.getBody();
+		Gson gson = new Gson();
+		String json = gson.toJson(stringJson);
 		
 		
-		System.out.println(jsonResponse.getBody());	
+		System.out.println(json);
+		//System.out.println(jsonResponse.getBody());	
 	}
 
 }
