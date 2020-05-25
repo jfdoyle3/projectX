80e478127acb330aa159fe837ba32312841b2c33
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2020-05-24 21:30:32 -0400

    Working on parsing data

--- DataProcessing/src/GetAPIData/PotHole.java
+++ DataProcessing/src/GetAPIData/PotHole.java
@@ -1,26 +1,109 @@
 package GetAPIData;
 
-public class PotHole {
-	
-	 String location;
-	 String ward;
-	 String datefilled;
-	 String totalfilled;
-	 String status;
-	//private String geolocation[];
-	// Field name:  :@computed_region_cjq3_kq3a
-	//private String computed_region_cjq3_kq3a;
-	// Field name:  :@computed_region_asue_2ipu
-	//private String computed_region_asue_2ipu;
-	// Field name:  :@computed_region_ueqj_g33x
-	//private String computed_region_ueqj_g33x;
+import java.util.Arrays;
 
-	public PotHole(String Location, String Ward, String DateFilled, String TotalFilled, String Status) {
-		
-		this.location=Location;
-		this.ward=Ward;
-		this.datefilled=DateFilled;
-		this.totalfilled=TotalFilled;
-		this.status=Status;
+public class PotHole {
+
+	public String location;
+	public String ward;
+	public String date_reported;
+	public String date_filled;
+	public String total_filled;
+	public String status;
+	//public String geolocation[];
+	// Field name: :@computed_region_cjq3_kq3a
+	public String computed_region_cjq3_kq3a;
+	// Field name: :@computed_region_asue_2ipu
+	public String computed_region_asue_2ipu;
+	// Field name: :@computed_region_ueqj_g33x
+	public String computed_region_ueqj_g33x;
+	
+//	public PotHole(String Location, String Ward, String date_filled, String total_filled, String Status) {
+//		
+//		this.location=Location;
+//		this.ward=Ward;
+//		this.date_filled=date_filled;
+//		this.total_filled=total_filled;
+//		this.status=Status;
+//	}
+	
+	public String getLocation() {
+		return location;
 	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getWard() {
+		return ward;
+	}
+	public void setWard(String ward) {
+		this.ward = ward;
+	}
+
+	public String getDate_reported() {
+		return date_reported;
+	}
+	public void setDate_reported(String date_reported) {
+		this.date_reported = date_reported;
+	}
+	public String getDate_filled() {
+		return date_filled;
+	}
+	public void setDate_filled(String date_filled) {
+		this.date_filled = date_filled;
+	}
+	public String gettotal_filled() {
+		return total_filled;
+	}
+	public void setTotal_filled(String total_filled) {
+		this.total_filled = total_filled;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+//	public String[] getGeolocation() {
+//		return geolocation;
+//	}
+//	public void setGeolocation(String[] geolocation) {
+//		this.geolocation = geolocation;
+//	}
+	public String getComputed_region_cjq3_kq3a() {
+		return computed_region_cjq3_kq3a;
+	}
+	public void setComputed_region_cjq3_kq3a(String computed_region_cjq3_kq3a) {
+		this.computed_region_cjq3_kq3a = computed_region_cjq3_kq3a;
+	}
+	public String getComputed_region_asue_2ipu() {
+		return computed_region_asue_2ipu;
+	}
+	public void setComputed_region_asue_2ipu(String computed_region_asue_2ipu) {
+		this.computed_region_asue_2ipu = computed_region_asue_2ipu;
+	}
+	public String getComputed_region_ueqj_g33x() {
+		return computed_region_ueqj_g33x;
+	}
+	public void setComputed_region_ueqj_g33x(String computed_region_ueqj_g33x) {
+		this.computed_region_ueqj_g33x = computed_region_ueqj_g33x;
+	}
+	
+//	@Override
+//	public String toString() {
+//		return "PotHole [location=" + location + ", ward=" + ward + ", date_reported=" + date_reported + ", date_filled="
+//				+ date_filled + ", total_filled=" + total_filled + ", status=" + status + ", geolocation="
+//				+ Arrays.toString(geolocation) + ", computed_region_cjq3_kq3a=" + computed_region_cjq3_kq3a
+//				+ ", computed_region_asue_2ipu=" + computed_region_asue_2ipu + ", computed_region_ueqj_g33x="
+//				+ computed_region_ueqj_g33x + "]";
+//	}
+	@Override
+	public String toString() {
+		return "PotHole [location=" + location + ", ward=" + ward + ", date_reported=" + date_reported + ", date_filled="
+				+ date_filled + ", total_filled=" + total_filled + ", status=" + status 
+				+ ", computed_region_asue_2ipu=" + computed_region_asue_2ipu + ", computed_region_ueqj_g33x="
+				+ computed_region_ueqj_g33x + "]";
+	}
+	
 }
--- DataProcessing/src/GetAPIData/PotHoleData.java
+++ DataProcessing/src/GetAPIData/PotHoleData.java
@@ -1,8 +1,16 @@
 package GetAPIData;
 
+
+import java.util.ArrayList;
+
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+
+
 
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
@@ -13,14 +21,27 @@ public class PotHoleData {
 
 	public static void getPotHoleData() {
 		
-		final HttpResponse<String> jsonResponse = Unirest.get("https://data.providenceri.gov/resource/tisk-wsvu.json").asString();
-		String stringJson=jsonResponse.getBody();
+		String oneJson="{\"location\":\"Halsey St\",\"ward\":\"2\",\"date_reported\":\"2016-06-07T00:00:00.000\",\"status\":\"Closed\"}";
+		ArrayList<PotHole> potholeList=new ArrayList<PotHole>();
+		
+	//	final HttpResponse<JsonNode> jsonNodeResponse = Unirest.get("https://data.providenceri.gov/resource/tisk-wsvu.json").asJson();
+		final HttpResponse<String> jsonStringResponse = Unirest.get("https://data.providenceri.gov/resource/tisk-wsvu.json").asString();
+		
+		String stringJson=jsonStringResponse.getBody();
+		String[] arrJson=stringJson.split("\\n");
 		Gson gson = new Gson();
-		String json = gson.toJson(stringJson);
+		//String json = gson.toJson(arrJson[0]);
+		//System.out.println(arrJson[]);
 		
+		for(int idx=0; idx<arrJson.length; idx++) {
+			PotHole data = new Gson().fromJson(arrJson[idx], PotHole.class);
+			potholeList.add(data);
+		}
+		//PotHole address=gsonBuild.fromJson(json, PotHole.class);
 		
-		System.out.println(json);
-		//System.out.println(jsonResponse.getBody());	
+		//System.out.println(data.toString());
+		
+		// System.out.println(json);
 	}
 
 }
